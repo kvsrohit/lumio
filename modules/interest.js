362238f6a9e0b47962911d7e72fe3937a521260d
Rohit Khode <kvsrohit@yahoo.com>
2018-06-23 16:27:13 +0530

    schedule display v1

--- modules/interest.js
+++ modules/interest.js
@@ -1,9 +1,9 @@
 function _getNextDate(loan, current) {
   var nextDate = new Date(current.getTime());
-  if(nextDate.getUTCDate()>=loan.interestDay){
+  if (nextDate.getUTCDate() >= loan.interestDay) {
     nextDate.setUTCMonth(current.getUTCMonth() + 1);
   }
-  nextDate.setUTCDate(loan.interestDay);    
+  nextDate.setUTCDate(loan.interestDay);
   return nextDate;
 }
 
@@ -11,10 +11,11 @@ function _getDays(start, end) {
   return Math.floor((end - start) / (24 * 3600 * 1000));
 }
 
-function round(num){
+function round(num) {
   //return Math.round( num * 100 + Number.EPSILON ) / 100;
   return Math.round(num);
 }
+
 function calculateInterest(loan, start, end) {
   var days = _getDays(start, end);
   var periodLength = loan.interestFrequency === 'Monthly' ? 30 : 360;
@@ -31,14 +32,22 @@ function stubApplicable(loan) {
 }
 
 function generateSchedule(loan, calcDate) {
+  var schedule = {
+    interests: [],
+    accrued: {
+      value: 0,
+      date: calcDate
+    }
+  };
+  if (!loan || !loan.startDate || !loan.amount) {
+    return schedule;
+  }
   if (typeof loan.startDate === 'string') {
     loan.startDate = new Date(loan.startDate);
   }
   var startDate = loan.startDate;
   var currentDate = startDate;
   var nextDate = _getNextDate(loan, currentDate);
-
-  var schedule = {interests:[], accrued:{value: 0, date: calcDate}};
 
   if ((nextDate > startDate) && (nextDate < calcDate) && stubApplicable(loan)) {
     var interest = calculateInterest(loan, startDate, nextDate);
