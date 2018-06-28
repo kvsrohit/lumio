f00e12d534ece3fcad7b2c203e62de4ce5c27840
Rohit Khode <kvsrohit@yahoo.com>
2018-06-28 08:02:48 +0530

    interest calculations to consider principal flows

--- modules/interest.js
+++ modules/interest.js
@@ -7,6 +7,12 @@ function _getNextDate(loan, current) {
   return nextDate;
 }
 
+function _addDays(start, n) {
+  //var ret = new Date(start.getTime());
+  //ret.setUTCDate(ret.getUTCDate()+n);
+  return new Date(start.getTime()+(n*24*3600*1000));
+}
+
 function _getDays(start, end) {
   return Math.floor((end - start) / (24 * 3600 * 1000));
 }
@@ -16,15 +22,22 @@ function round(num) {
   return Math.round(num);
 }
 
-function calculateInterest(loan, start, end) {
-  var days = _getDays(start, end);
-  var periodLength = loan.interestFrequency === 'Monthly' ? 30 : 360;
-  return round(loan.amount * loan.interestRate * (days / periodLength) / 100);
+function calculateInterestForLoan(loan, start, end){
+  return calculateInterest(loan.amount, loan.interestRate, loan.interestFrequency, start, end);
+}
 
+function calculateInterest(amount, rate, frequency, start, end) {
+  var days = _getDays(start, end);
+  var periodLength = frequency === 'Monthly' ? 30 : 360;
+  return getInterestForPeriod(amount, rate, days/periodLength);
+}
+
+function getInterestForPeriod(amount, rate, period) {
+  return round(amount * rate * period / 100);
 }
 
 function getFullPeriodInterest(loan) {
-  return loan.amount * loan.interestRate / 100;
+  return getInterestForPeriod(loan.amount, loan.interestRate, 1);
 }
 
 function stubApplicable(loan) {
@@ -32,10 +45,11 @@ function stubApplicable(loan) {
 }
 
 function generateSchedule(loan, calcDate) {
+
   var schedule = {
     interests: [],
     accrued: {
-      value: 0,
+      amount: 0,
       date: calcDate
     }
   };
@@ -47,22 +61,35 @@ function generateSchedule(loan, calcDate) {
   }
   var startDate = loan.startDate;
   loan.interestDay = loan.interestDay || startDate.getUTCDate();
+
+  var outstandingPrincipal = loan.amount;
+  var principalFlows = loan.principalFlows?loan.principalFlows.slice():[];
+
   var currentDate = startDate;
   var nextDate = _getNextDate(loan, currentDate);
 
   if ((nextDate > startDate) && (nextDate < calcDate) && stubApplicable(loan)) {
-    var interest = calculateInterest(loan, startDate, nextDate);
+    var interest = calculateInterest(outstandingPrincipal, loan.interestRate, loan.interestFrequency, startDate, nextDate);
     schedule.interests.push({
-      value: interest,
+      principal: outstandingPrincipal,
+      amount: interest,
       date: nextDate
     });
     currentDate = nextDate;
     nextDate = _getNextDate(loan, nextDate);
   }
-  var periodInterest = getFullPeriodInterest(loan);
+  var periodInterest = getInterestForPeriod(outstandingPrincipal, loan.interestRate, 1);
   while (nextDate <= calcDate) {
+
+    while(principalFlows.length>0 && principalFlows[0].date < _addDays(currentDate, loan.leasePeriod)){
+      outstandingPrincipal -= principalFlows[0].amount;
+      principalFlows.shift();
+      periodInterest = getInterestForPeriod(outstandingPrincipal, loan.interestRate, 1);
+    }
+
     schedule.interests.push({
-      value: periodInterest,
+      principal: outstandingPrincipal,
+      amount: periodInterest,
       date: nextDate
     });
     currentDate = nextDate;
@@ -71,16 +98,77 @@ function generateSchedule(loan, calcDate) {
 
   if (calcDate > currentDate) {
     //console.log('Add End Stub');
-    schedule.accrued.value = calculateInterest(loan, currentDate, calcDate);
+    schedule.accrued.amount = calculateInterest(outstandingPrincipal, loan.interestRate, loan.interestFrequency, currentDate, calcDate);
+    schedule.accrued.principal = outstandingPrincipal;
   }
-
+  schedule.outstandingPrincipal = outstandingPrincipal;
   return schedule;
 }
+
+function _prepareFlows(flows){
+  flows = flows || [];
+  flows.forEach(function(v) {
+    if(typeof v.date === 'string'){
+      v.date = new Date(v.date);
+    }
+  });
+  flows.sort(function(a, b){
+    return a.date > b.date;
+  });
+  return flows;
+}
+
+function getAnalytics(loan, calcDate) {
+
+  /* Prepare the loan for dates and payment sorting */
+  if (typeof loan.startDate === 'string') {
+    loan.startDate = new Date(loan.startDate);
+  }
+  /* Prepare the value-date data-type and sort the principal flows */
+  loan.principalFlows = _prepareFlows(loan.principalFlows);
+  loan.interestFlows = _prepareFlows(loan.interestFlows);
+  
+  var schedule = generateSchedule(loan, calcDate);
+  var interestFlows = loan.interestFlows.slice();
+  var overPayment = 0; /* Residue from previous additional payment */
+  schedule.outstandingInterest = 0;
+  schedule.interests.forEach(function(item) {
+    item.status = 'pending';
+    var outstanding = item.amount-overPayment;
+
+    if(outstanding>0){
+      overPayment = 0;
+      while(outstanding>0 && interestFlows.length>0){
+        item.status = 'partial';
+        outstanding -= interestFlows[0].amount;
+        interestFlows.shift();
+        if(outstanding<0) {
+          overPayment = -outstanding;
+          outstanding = 0;
+          item.status = 'paid';
+        }
+      }
+      item.outstanding = outstanding;
+    } else {
+      item.outstanding = 0;
+      item.status = 'paid';
+      overPayment = -outstanding;
+    }
+    schedule.outstandingInterest += item.outstanding;
+  });
+  schedule.overPayment = overPayment;
+  schedule.outstandingAmount = schedule.outstandingPrincipal + schedule.outstandingInterest - schedule.overPayment;
+  return schedule;
+}
+
+
+
 
 module.exports = {
   round: round,
   stubApplicable: stubApplicable,
   calculateInterest: calculateInterest,
   getFullPeriodInterest: getFullPeriodInterest,
-  generateSchedule: generateSchedule
+  generateSchedule: generateSchedule,
+  getAnalytics: getAnalytics
 };
--- test/interest-tests.js
+++ test/interest-tests.js
@@ -42,14 +42,14 @@ describe('Interest Calculation', function () {
 
   it('calculateInterest: returns 0 day\'s interest when start and end are same', function () {
     var start = new Date();
-    var interest = Interest.calculateInterest(loan, start, start);
+    var interest = Interest.calculateInterest(loan.amount, loan.interestRate, loan.interestFrequency, start, start);
     expect(interest).to.equal(0);
   });
 
   it('calculateInterest: returns 7 day\'s interest', function () {
     var start = dateAfter(loan.startDate, 10);
     var end = dateAfter(start, 7);
-    var interest = Interest.calculateInterest(loan, start, end);
+    var interest = Interest.calculateInterest(loan.amount, loan.interestRate, loan.interestFrequency, start, end);
     var expected = Interest.round(loan.amount * loan.interestRate * (7 / 30) / 100);
     expect(interest).to.equal(expected);
   });
@@ -58,7 +58,7 @@ describe('Interest Calculation', function () {
     var start = dateAfter(loan.startDate, 10);
     var end = dateAfter(start, 58);
     loan.interestFrequency = 'Annual';
-    var interest = Interest.calculateInterest(loan, start, end);
+    var interest = Interest.calculateInterest(loan.amount, loan.interestRate, loan.interestFrequency, start, end);
     var expected = Interest.round(loan.amount * loan.interestRate * (58 / 360) / 100);
     expect(interest).to.equal(expected);
   });
@@ -92,7 +92,7 @@ describe('Interest Calculation', function () {
     expect(schedule.interests.length).to.equal(6);
     expect(schedule.interests[0].date).to.deep.equal(new Date('2018-03-01T00:00:00.000Z'));
     var expected = Interest.round(loan.amount * loan.interestRate * (19 / 30) / 100);
-    expect(schedule.interests[0].value).to.equal(expected);
+    expect(schedule.interests[0].amount).to.equal(expected);
   });
   
   it('generateSchedule: generates period interest', function () {
@@ -103,23 +103,23 @@ describe('Interest Calculation', function () {
     expect(schedule.interests.length).to.equal(6);
     expect(schedule.interests[1].date).to.deep.equal(new Date('2018-04-01T00:00:00.000Z'));
     var expected = Interest.round(loan.amount * loan.interestRate / 100);
-    expect(schedule.interests[1].value).to.equal(expected);
+    expect(schedule.interests[1].amount).to.equal(expected);
   });
 
   it('generateSchedule: generates end stub interest ', function () {
     var calculationDate = new Date('2018-08-27T00:00:00.000Z');
-    var schedule = Interest.generateSchedule(loan, calculationDate, true);
+    var schedule = Interest.generateSchedule(loan, calculationDate);
     expect(schedule).to.be.ok;
     expect(schedule.accrued).to.be.ok;
     expect(schedule.accrued.date).to.deep.equal(new Date('2018-08-27T00:00:00.000Z'));
     var expected = Interest.round(loan.amount * loan.interestRate * (26 / 30) / 100);
-    expect(schedule.accrued.value).to.equal(expected);
+    expect(schedule.accrued.amount).to.equal(expected);
   });
 
   it('generateSchedule: generates correctly when interest payment date is different', function () {
     var calculationDate = new Date('2018-08-27T00:00:00.000Z');
     loan.interestDay = 18;
-    var schedule = Interest.generateSchedule(loan, calculationDate, true);
+    var schedule = Interest.generateSchedule(loan, calculationDate);
     expect(schedule).to.be.ok;
     expect(schedule.interests).to.not.be.empty;
     expect(schedule.interests.length).to.equal(7);
@@ -127,7 +127,7 @@ describe('Interest Calculation', function () {
     /* First Stub 10th - to - 18th Feb */
     expect(schedule.interests[0].date).to.deep.equal(new Date('2018-02-18T00:00:00.000Z'));
     var expected = Interest.round(loan.amount * loan.interestRate * (8 / 30) / 100);
-    expect(schedule.interests[0].value).to.equal(expected);
+    expect(schedule.interests[0].amount).to.equal(expected);
 
     /* Period Interest */
     expect(schedule.interests[1].date).to.deep.equal(new Date('2018-03-18T00:00:00.000Z'));
@@ -136,21 +136,77 @@ describe('Interest Calculation', function () {
     /* End Stub 18th - to - 27th Aug */
     expect(schedule.accrued.date).to.deep.equal(new Date('2018-08-27T00:00:00.000Z'));
     expected = Interest.round(loan.amount * loan.interestRate * (9 / 30) / 100);
-    expect(schedule.accrued.value).to.equal(expected);
+    expect(schedule.accrued.amount).to.equal(expected);
   });
 
   it('generateSchedule: generates correctly on interest payment date', function () {
     var calculationDate = new Date('2018-08-18T00:00:00.000Z');
     loan.interestDay = 18;
-    var schedule = Interest.generateSchedule(loan, calculationDate, true);
+    var schedule = Interest.generateSchedule(loan, calculationDate);
     expect(schedule).to.be.ok;
     expect(schedule.interests).to.not.be.empty;
     expect(schedule.interests.length).to.equal(7);
     expect(schedule.interests[schedule.interests.length-1].date).to.deep.equal(new Date('2018-08-18T00:00:00.000Z'));
     var expected = Interest.round(loan.amount * loan.interestRate / 100);
-    expect(schedule.interests[schedule.interests.length-1].value).to.equal(expected);
-    expect(schedule.accrued.value).to.equal(0);
-    
+    expect(schedule.interests[schedule.interests.length-1].amount).to.equal(expected);
+    expect(schedule.accrued.amount).to.equal(0);
+  });
+
+  it('generateSchedule: applies the principal-flows correctly', function(){
+    var calculationDate = new Date('2018-09-18T00:00:00.000Z');
+    loan.principalFlows = [{
+      date: new Date('2018-04-04T00:00:00.000Z'),
+      amount: 5000
+    }, {
+      date: new Date('2018-06-11T00:00:00.000Z'),
+      amount: 5000
+    }, {
+      date: new Date('2018-07-28T00:00:00.000Z'),
+      amount: -4000
+    }, {
+      date: new Date('2018-10-11T00:00:00.000Z'),
+      amount: 5000
+    }];
+    var schedule = Interest.generateSchedule(loan, calculationDate);
+
+    /* All flows except the last flow outside the calculation period 
+     * are applied */
+    expect(schedule.outstandingPrincipal).to.equal(14000);
+
+    /* Original principal is used until the change-date*/
+    expect(schedule.interests[1].amount).to.equal(400);
+    expect(schedule.interests[1].principal).to.equal(20000);
+
+    /* The principal repayment within the lease-period is applied immediately */
+    expect(schedule.interests[2].amount).to.equal(300);
+    expect(schedule.interests[2].principal).to.equal(15000);
+
+    /* The principal repayment outside the lease-period is applied next month */
+    expect(schedule.interests[5].amount).to.equal(200);
+    expect(schedule.interests[5].principal).to.equal(10000);
+
+    /* The principal increase is considered correctly */
+    expect(schedule.interests[6].amount).to.equal(280);
+    expect(schedule.interests[6].principal).to.equal(14000);    
+  });
+
+  it('getAnalytics: applies the interest-flows correctly', function(){
+    var calculationDate = new Date('2018-07-18T00:00:00.000Z');
+    loan.interestFlows = [{
+      date: new Date('2018-04-04T00:00:00.000Z'),
+      amount: 300
+    }, {
+      date: new Date('2018-05-11T00:00:00.000Z'),
+      amount: 400
+    }, {
+      date: new Date('2018-06-28T00:00:00.000Z'),
+      amount: 450
+    }, {
+      date: new Date('2018-06-28T00:00:00.000Z'),
+      amount: 650
+    }];
+    var schedule = Interest.getAnalytics(loan, calculationDate);
+    console.log(schedule);
   });
 
 });
